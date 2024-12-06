a906875e9d348c21a61875ecda45176b30aa4c94
wmvalente <63470082+wmvalente@users.noreply.github.com>
2024-12-06 17:54:30 -0300

    Update userdata.sh

--- infrastructure/aws/compute/userdata.sh
+++ infrastructure/aws/compute/userdata.sh
@@ -3,4 +3,4 @@ apt-get update -y
 apt-get install -y nginx
 systemctl enable nginx
 systemctl start nginx
-#echo "You reached the server" > /var/www/html/index.html
+#echo "You reached the server Dude" > /var/www/html/index.html
